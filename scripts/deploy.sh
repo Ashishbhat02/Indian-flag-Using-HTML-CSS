b7c90129d0abd313aed3e0bb725b051180e405c9
Ashishbhat02 <bhatashish755@gmail.com>
2025-03-20 16:15:47 +0530

    update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-sudo cp -r * /var/www/html/
+cd /var/www/html/
+mkdir -p temp
+
+# Copy the Indian_Flag.html file from the temp folder to /var/www/html/
+cp temp/Indian_Flag.html /var/www/html/
+
+# Remove the temp folder after copying
+rm -rf temp
+
+# Restart Apache to apply changes
 sudo systemctl restart apache2
