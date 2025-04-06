a5497dd1882eb5dc89698e29c72075de2c391a7f
Ashish Bhat <123158100+Ashishbhat02@users.noreply.github.com>
2025-04-06 17:11:41 +0530

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-cd /var/www/django/templates/
+cd /var/www/myproject/templates/
 # rm -rf index.html
 mkdir -p temp
 
 # Copy the Indian_Flag.html file from the temp folder to /var/www/html/
-cp temp/Indian_Flag.html /var/www/django/templates/
+cp temp/Indian_Flag.html /var/www/myproject/templates/
 
 # Remove the temp folder after copying
 rm -rf temp
