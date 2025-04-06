0f9e505c25b9ba9a6ada4b401f9c6864ba3b485f
Ashish Bhat <123158100+Ashishbhat02@users.noreply.github.com>
2025-04-06 17:10:08 +0530

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
-
-cd /var/www/myproject/templates/
+cd /var/www/django/templates/
 # rm -rf index.html
-sudo mkdir -p temp
-cd temp
+mkdir -p temp
 
 # Copy the Indian_Flag.html file from the temp folder to /var/www/html/
-cp temp/Indian_Flag.html /var/www/myproject/templates/
+cp temp/Indian_Flag.html /var/www/django/templates/
 
 # Remove the temp folder after copying
-sudo rm -rf temp
+rm -rf temp
 
 # Restart Apache to apply changes
 sudo systemctl restart apache2
