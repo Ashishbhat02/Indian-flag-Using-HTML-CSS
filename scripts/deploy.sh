4ea4fa589919bf76f17de15e09f27d76e5f5743b
Ashish Bhat <123158100+Ashishbhat02@users.noreply.github.com>
2025-04-06 16:46:15 +0530

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,8 +4,6 @@ cd /var/www/myproject/templates/
 # rm -rf index.html
 sudo mkdir -p temp
 
-cd /var/www/myproject/templates/temp
-
 # Copy the Indian_Flag.html file from the temp folder to /var/www/html/
 cp temp/Indian_Flag.html /var/www/myproject/templates/
 
