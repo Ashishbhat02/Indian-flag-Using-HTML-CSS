38e8ca01096dc3300065fc4252c3b25e5723b517
Ashish Bhat <123158100+Ashishbhat02@users.noreply.github.com>
2025-04-06 16:36:43 +0530

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,13 +2,13 @@
 
 cd /var/www/myproject/templates/
 # rm -rf index.html
-mkdir -p tem
+mkdir -p temp
 
 # Copy the Indian_Flag.html file from the temp folder to /var/www/html/
-cp tem/Indian_Flag.html /var/www/myproject/templates/
+cp temp/Indian_Flag.html /var/www/myproject/templates/
 
 # Remove the temp folder after copying
-rm -rf tem
+# rm -rf temp
 
 # Restart Apache to apply changes
 sudo systemctl restart apache2
