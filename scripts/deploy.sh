aac56575240c0aa2f9558aa260e85e1d7529ff6a
Ashish Bhat <123158100+Ashishbhat02@users.noreply.github.com>
2025-04-07 15:09:44 +0530

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,9 +2,8 @@
 cd /var/www/django
 
 # Copy the Indian_Flag.html file from the temp folder to /var/www/html/
-cp Indian_Flag.html /var/www/django/templates/
+sudo cp Indian_Flag.html /var/www/django/templates/
 
-rm -rf Indian-Flag.html
 
 # Restart Apache to apply changes
 sudo systemctl restart apache2
